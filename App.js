4005ce10b4f97858f398e76bdd779934af939b0b
Charpell <charpellumeh@gmail.com>
2018-08-12 22:04:07 +0100

    Configure screen for ios

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header />
+        <View style={styles.content}>
+
+        </View>
+        <Footer />
       </View>
     );
   }
@@ -16,8 +21,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5F5F5",
+    ...Platform.select({
+      ios: {
+        paddingTop: 30,
+      }
+    })
+    
   },
-});
+  content: {
+    flex: 1
+  }
+})
