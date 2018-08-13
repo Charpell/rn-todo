7297170847cb888712d409b8c39e48fa47ae482a
Charpell <charpellumeh@gmail.com>
2018-08-13 09:36:58 +0100

    Create TextInput component to receive input

--- App.js
+++ App.js
@@ -5,10 +5,40 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    }
+  }
+
+  handleAddItem = () => {
+    if (!this.state.value) return
+
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ]
+    
+    this.setState({
+      items: newItems,
+      value: ""
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <Header
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={(value) => this.setState({ value })}
+         />
         <View style={styles.content}>
 
         </View>
